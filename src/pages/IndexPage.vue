eadb56be6378c852ef54841e53f75cb8b80ccfe3
Isrrael Bolivar <isrraelbolivar77@gmail.com>
2023-07-31 17:01:32 -0400

    Subiendo prueba

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+   <div class="text-h1" color="green">
+       Prueba
+  </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
